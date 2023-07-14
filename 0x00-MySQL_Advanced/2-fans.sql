1083960bba439b287544e476b22fa855afa67766
AniKanayo <micanian4real@gmail.com>
2023-07-14 23:24:45 +0000

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,11 +1,11 @@
--- Creating the view 'band_ranking' that contains the ranking of countries by the number of (non-unique) fans
-CREATE OR REPLACE VIEW band_ranking AS
+-- Group by origin and sum the number of fans
 SELECT 
-    origin,  -- The 'origin' column containing the country of origin
-    SUM(fans) AS nb_fans  -- The 'nb_fans' column containing the total number of (non-unique) fans
-FROM
-    metal_bands
-GROUP BY
-    origin
-ORDER BY
-    nb_fans DESC;
+  origin, -- The 'origin' column showing the country of origin
+  SUM(fans) AS nb_fans  -- The 'nb_fans' column representing the total number of (non-unique) fans
+FROM 
+  metal_bands
+GROUP BY 
+  origin
+ORDER BY 
+  nb_fans DESC -- Order the results by the number of fans in descending order
+;
