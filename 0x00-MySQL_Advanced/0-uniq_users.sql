144875ea1f7ff4b888e95511a0b5ab8f369d5593
AniKanayo <micanian4real@gmail.com>
2023-07-14 22:10:08 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,10 @@
---database selection, replace 'database_name' with your database
-USE holberton;
-
--- table creation
+-- With the statement below we are ensuring that the table 'users' will only be created if it does not exist
 CREATE TABLE IF NOT EXISTS users (
+    -- Creating an 'id' column which will be an integer, can't be null, auto increments and is a Primary key
     id INT AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    -- Creating an 'email' column which is a string of 255 max length, can't be null and should be unique
+    email VARCHAR(255) NOT NULL UNIQUE,
+    -- Creating a 'name' column which is a string of 255 max length
     name VARCHAR(255),
-    PRIMARY KEY (id),
-    UNIQUE (email)
-);
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
