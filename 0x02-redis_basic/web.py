d5c8d1488c868aa24706718aacde5c6a8570475b
AniKanayo <micanian4real@gmail.com>
2023-07-19 22:08:08 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,70 +1,37 @@
+#!/usr/bin/env python3
+"""A module that contains functionality for webpage fetching and caching"""
+
 import requests
-import time
-from cachetools import TTLCache
-from typing import Dict
-
-# Create a cache with an expiration time of 10 seconds
-cache = TTLCache(maxsize=100, ttl=10)
+from Cache import Cache  # import the Cache class
 
 
-def get_page(url: str) -> str:
+def cache_request(fn):
+    """ A decorator for caching the result of a function call
+    Args:
+    fn: The function to be decorated
     """
-    Fetches the HTML content of a given URL using requests
-    module and caches the result.
-
-    Parameters:
-        url (str): The URL to fetch the HTML content from.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    if url in cache:
-        return cache[url]
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.text
-        cache[url] = html_content
-        return html_content
-    else:
-        raise requests.HTTPError(f"Failed to fetch URL: {url}")
-
-
-def cached_page_count() -> Dict[str, int]:
-    """
-    Returns a dictionary containing the count of cached pages for each URL.
-
-    Returns:
-        Dict[str, int]: A dictionary containing the count of cached
-        pages for each URL.
-    """
-    return {key: cache.stats(key).current for key in cache.keys()}
-
-
-# Decorator for caching the result of the get_page function
-def cached_get_page(func):
     def wrapper(url):
-        if url in cache:
-            return cache[url]
-
-        html_content = func(url)
-        cache[url] = html_content
-        return html_content
-
+        cache = Cache()
+        result = cache.get(url)
+        key = "count:{}".format(url)
+        if result is not None:
+            cache._redis.incr(key)
+            return result
+        else:
+            result = fn(url)
+            cache._redis.set(url, result, ex=10)
+            cache._redis.set(key, 1, ex=10)
+            return result
     return wrapper
 
 
-@cached_get_page
-def slow_get_page(url: str) -> str:
-    """
-    Fetches the HTML content of a given URL using requests module
-    and caches the result with the decorator.
-
-    Parameters:
-        url (str): The URL to fetch the HTML content from.
-
+@cache_request
+def get_page(url: str) -> str:
+    """Fetches the webpage content of the specified URL
+    Args:
+        url: The URL of the webpage
     Returns:
-        str: The HTML content of the URL.
-    """
-    time.sleep(2)  # Simulate a slow response
-    return get_page(url)
+        The HTML content of the webpage as a string
+  """
+    response = requests.get(url)
+    return response.text
