52a5adc9b970764a5a8b1c57f3d5745d42f07357
AniKanayo <micanian4real@gmail.com>
2023-07-20 00:57:18 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,22 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """decorator to store the history of inputs and outputs
+    for a particular function"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Append the input arguments and the output to lists in Redis"""
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(inputs_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(result))
+        return result
+
+    return wrapper
+
+
 class Cache:
     """
     Cache class for a basic caching system
@@ -31,9 +47,13 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store the input data in Redis using a random key and return the key
+        Store the input data in Redis using a random key and return the key.
+        This method is decorated with 'count_calls' to count the number
+        of times it is called,
+        and 'call_history' to keep a history of its inputs and outputs.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -43,8 +63,8 @@ class Cache:
             Union[str, bytes, int, float]:
         """
         The get method retrieves data from redis by key
-        It can return the data in different formats depending on
-        the function supplied
+        It can return the data in different formats depending
+        on the function supplied
         """
         data = self._redis.get(key)
         if fn is None or data is None:
