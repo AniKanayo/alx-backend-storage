ba511cbd6d10b62b8ccf53208d88c8569530dfa8
AniKanayo <micanian4real@gmail.com>
2023-07-20 00:04:35 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,48 +7,23 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """Decorator to count the number of times a method is called."""
-    key = f'{method.__qualname__}'
+    """decorator to count the number of times a method is called"""
+    key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Increment key value every time a method is called."""
+        """Increment key value every time a method is called"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
 
     return wrapper
 
 
-def call_history(method: Callable) -> Callable:
-    """Decorator to store the history of method inputs and outputs."""
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        # Creating keys for storage:
-        inputs_key = f'{method.__qualname__}:inputs'
-        outputs_key = f'{method.__qualname__}:outputs'
-
-        # String to save in Redis as inputs
-        str_args = ', '.join(map(str, args))
-
-        # Use RPUSH to add inputs to Redis
-        self._redis.rpush(inputs_key, str_args)
-
-        # Compute the output of the method
-        output = method(self, *args, **kwargs)
-
-        # Use RPUSH to add output to Redis
-        self._redis.rpush(outputs_key, str(output))
-
-        return output
-
-    return wrapper
-
-
 class Cache:
     """
-    Cache class for a basic caching system.
+    Cache class for a basic caching system
     Args:
-        data: The data which will be passed as a str, bytes, int or float.
+        data: the data which will be passed as a str, bytes, int or float.
     """
     def __init__(self):
         """ Instantiate a redis object and applies a flushdb() """
@@ -56,10 +31,10 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
-    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store the input data in Redis using a random key and
-        return the key."""
+        """
+        Store the input data in Redis using a random key and return the key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -67,7 +42,7 @@ class Cache:
     def get(self, key: str, fn: Optional[Callable] = None) -> \
             Union[str, bytes, int, float]:
         """
-        The get method retrieves data from redis by key.
+        The get method retrieves data from redis by key
         It can return the data in different formats depending
         on the function supplied
         """
