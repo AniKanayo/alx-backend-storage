baf3074eb4a4a2f53532d594ab5732a4f4ec56b4
AniKanayo <micanian4real@gmail.com>
2023-07-20 01:50:55 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,52 +1,62 @@
 #!/usr/bin/env python3
-"""
-Module Documentation
-A module that handles web page caching and access counting.
-"""
 
 import requests
-import redis
-from typing import Callable
-from functools import wraps
+import time
+from cachetools import TTLCache, cached
+from typing import Dict
 
-r = redis.Redis()
-
-
-def count_url_calls(func: Callable) -> Callable:
-    """
-    Decorator for counting how many times a URL has been accessed.
-    """
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        key = f'count:{url}'
-        r.incr(key)
-        return func(url)
-
-    return wrapper
+# Create a cache with an expiration time of 10 seconds
+cache = TTLCache(maxsize=100, ttl=10)
 
 
-def cache_response(func: Callable) -> Callable:
-    """
-    Decorator for caching a function's response.
-    """
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        key = f'cache:{url}'
-        cached_response = r.get(key)
-        if cached_response is not None:
-            return cached_response
-        response = func(url)
-        r.setex(key, 10, response)
-        return response
-
-    return wrapper
-
-
-@count_url_calls
-@cache_response
+@cached(cache)
 def get_page(url: str) -> str:
     """
-    Function for retrieving the HTML content of a web page.
+    Fetches the HTML content of a given URL using requests module and
+    caches the result.
+
+    Parameters:
+        url (str): The URL to fetch the HTML content from.
+
+    Returns:
+        str: The HTML content of the URL.
     """
+    count_key = f"count:{url}"
+
+    if count_key in cache:
+        cache[count_key] += 1
+    else:
+        cache[count_key] = 1
+
     response = requests.get(url)
-    return response.text
+    if response.status_code == 200:
+        html_content = response.text
+        return html_content
+    else:
+        raise requests.HTTPError(f"Failed to fetch URL: {url}")
+
+
+def cached_page_count() -> Dict[str, int]:
+    """
+    Returns a dictionary containing the count of cached pages for each URL.
+
+    Returns:
+        Dict[str, int]: A dictionary containing the count of cached pages
+        for each URL.
+    """
+    return {key: cache.stats(key).current for key in cache.keys()}
+
+
+if __name__ == "__main__":
+    # Test the get_page function with a slow URL
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000"
+    "/url/https://example.com"
+    start_time = time.time()
+    content = get_page(slow_url)
+    end_time = time.time()
+    print(f"Time taken to fetch the content:"
+          "{end_time - start_time:.2f} seconds")
+
+    # Test the cached_page_count function to get the count of cached pages
+    print("Cached page count:")
+    print(cached_page_count())
