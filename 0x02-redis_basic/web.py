fb9cb187bb2f1d31b5ab78c65731feca4dee97dc
AniKanayo <micanian4real@gmail.com>
2023-07-20 01:10:38 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python3
-"""A module that contains functionality for webpage fetching and caching"""
+"""
+Module Documentation
+A module that handles web page caching and access counting.
+"""
 
 import requests
-from Cache import Cache  # import the Cache class
+import redis
+from typing import Callable
+from functools import wraps
+
+r = redis.Redis()
 
 
-def cache_request(fn):
-    """ A decorator for caching the result of a function call
-    Args:
-    fn: The function to be decorated
+def count_url_calls(func: Callable) -> Callable:
     """
-    def wrapper(url):
-        cache = Cache()
-        result = cache.get(url)
-        key = "count:{}".format(url)
-        if result is not None:
-            cache._redis.incr(key)
-            return result
-        else:
-            result = fn(url)
-            cache._redis.set(url, result, ex=10)
-            cache._redis.set(key, 1, ex=10)
-            return result
+    Decorator for counting how many times a URL has been accessed.
+    """
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        key = f'count:{url}'
+        r.incr(key)
+        return func(url)
+
     return wrapper
 
 
-@cache_request
+def cache_response(func: Callable) -> Callable:
+    """
+    Decorator for caching a function's response.
+    """
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        key = f'cache:{url}'
+        cached_response = r.get(key)
+        if cached_response is not None:
+            return cached_response
+        response = func(url)
+        r.setex(key, 10, response)
+        return response
+
+    return wrapper
+
+
+@count_url_calls
+@cache_response
 def get_page(url: str) -> str:
-    """Fetches the webpage content of the specified URL
-    Args:
-        url: The URL of the webpage
-    Returns:
-        The HTML content of the webpage as a string
-  """
+    """
+    Function for retrieving the HTML content of a web page.
+    """
     response = requests.get(url)
     return response.text
